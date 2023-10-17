36728a23d4ee8d6ae4d84722e72a41fe3dbe7a82
Valerie4792 <vaguilar4792@gmail.com>
2023-10-16 18:18:27 -0700

    updated contact page

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,31 +1,48 @@
 import React from 'react'
-import {Button, Col, Row, Nav, Form,} from 'react-bootstrap'
+import {Button, Col, Row, Nav, Form,Container } from 'react-bootstrap'
 import Nav1 from './Nav1'
 
 const Contact = () => {
   return (
     <>
     <Nav1/>
-    <Form style={{width:'40',marginLeft:"33%",marginTop: "10%",backgroundColor: "lightseagreen",padding:80, borderRadius: 5,}}>
-    <h1 className='text-center'>Login</h1>
-  <Form.Group className="mb-3" controlId="Username">
-    <Form.Label>Username</Form.Label>
-    <Form.Control type="text" placeholder="Enter Username" />
-    
-  </Form.Group>
+  <Container className='m-5 p-5'>
+    <Row>
+      <Col>
+      <h1 className='text-center m-5'>Let's Connect & Create Something Great!</h1>
+      <h3 className='text-center m-5'>Tell me about your project</h3>
+      <a href=""></a>
 
-  <Form.Group className="mb-3" controlId="formBasicPassword">
-    <Form.Label>Password</Form.Label>
-    <Form.Control type="password" placeholder="Password"  />
-  </Form.Group>
+      
+      </Col>
+      <Col className='m-5'>
+          <Form className="formContact">
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form.Label>First Name</Form.Label>
+        <Form.Control type="text" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form.Label>Last Name</Form.Label>
+        <Form.Control type="text" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="name@example.com" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+        <Form.Label>Example textarea</Form.Label>
+        <Form.Control as="textarea" rows={3} />
+      </Form.Group>
+    </Form>
+   <Button>Submit</Button>
+      </Col>
+    </Row>
+
+  </Container>
+    
   
-  <Button variant="outline-primary">
-    Submit
-  </Button>
-  <p className='mt-2'> If you don't have an account</p>
+</>
   
-</Form>
-    </>
     
   )
 }
